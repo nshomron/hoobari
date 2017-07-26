0342dc6c270309dc5d588bf78afd6f9a60e7c6d7
Guy Shapira <guyshapira@mail.tau.ac.il>
2017-07-26 16:21:44 +0300

    Fixed syntax errors in SQL commands

--- src/db.py
+++ src/db.py
@@ -1,42 +1,48 @@
 import sqlite3
-
+import csv
+from sys import stderr
 
 class Variants(object):
     def __init__(self,dbname='/groups/nshomron/guyshapira/projects/hoobari/src/hoobari.db'):
         # Connect to DB
-        con=sqlite3.connect(dbname,isolation_level=None)
+        self.con=sqlite3.connect(dbname,isolation_level=None)
 
         # Check table's existance
-        res=con.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='variants';")
+        res=self.con.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='variants';")
         if not res.fetchone():
-            con.execute('CREATE TABLE `variants` (`pos` int(10) NOT NULL,`chromosome` char(2) DEFAULT NULL,`genotype` char(1) DEFAULT NULL,`qname` varchar(50) DEFAULT NULL)')
+            self.con.execute('CREATE TABLE `variants` (`pos` int(10) NOT NULL,`chromosome` char(2) DEFAULT NULL,`genotype` char(1) DEFAULT NULL,`qname` varchar(50) DEFAULT NULL)')
 
     # Insert variants to table
-    def insertVariant(genotypes, chromosome, position):
+    def insertVariants(self,genotypes, chromosome, position):
         query='''
             INSERT INTO `variants`
             (`pos`,
             `chromosome`,
             `genotype`,
-            `qname`)
+            `qname`,
+            `length`)
             VALUES
-            (
             '''
         for genotype in genotypes:
-            query+='({0},{1},{2},{3}),'.format(pos,chromosomes,genotype,qname)
-        con.execute(query[:-1] + ');')
+            query+='({0},"{1}","{2}","{3}",{4}),'.format(position,
+                    chromosome,genotype[0],genotype[2],genotype[1])
+        query=query[:-1] + ';'
+        print(query)
+        self.con.execute(query)
 
     # Insert variant to table
-    def insertVariant(pos,chromosome,genotype,qname):
-        con.execute('''
+    def insertVariant(length,pos,chromosome,genotype,qname):
+        self.con.execute('''
             INSERT INTO `variants`
             (`pos`,
             `chromosome`,
             `genotype`,
-            `qname`)
+            `qname`,
+            `length`)
             VALUES
             ({0},
             {1},
             {2},
-            {3});
-        '''.format(pos,chromosomes,genotype,qname))
+            {3},
+            {4});
+        '''.format(pos,chromosomes,genotype,qname,length))
