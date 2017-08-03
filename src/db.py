697ff6c23275d5a81ac72b365b70ce309feb7bbd
shep <guyspersonal@gmail.com>
2017-08-03 19:47:17 +0300

    Enable WAL mode in sqlite3

--- src/db.py
+++ src/db.py
@@ -3,7 +3,7 @@ import csv
 from sys import stderr
 
 class Variants(object):
-    def __init__(self, dropdb=False, dbpath='.'):
+    def __init__(self, dropdb=False, dbpath='.', wal=True):
         # Connect to DB
         self.con = sqlite3.connect(dbpath + '/hoobari.db', isolation_level = None)#, timeout=10)
 
@@ -13,13 +13,16 @@ class Variants(object):
 
         # Check table's existance
         res = self.con.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='variants';")
-        
+
         if not res.fetchone():
+            # Enable WAL for better concurrency
+            if wal:
+                self.con.execute('PRAGMA journal_mode=WAL')
             self.con.execute('CREATE TABLE `variants` (`chromosome` char(2) DEFAULT NULL,`pos` int(10) NOT NULL,`genotype` varchar(20) DEFAULT NULL,`length` int(10) DEFAULT NULL,`qname` varchar(50) DEFAULT NULL)')
 
     # Insert variants to table
     def insertVariant(self, chromosome, position, info_list):
-        
+
         query = '''
             INSERT INTO `variants`
             (`chromosome`,
@@ -29,11 +32,11 @@ class Variants(object):
             `qname`)
             VALUES
             '''
-        
+
         for line in info_list:
             query += '("{0}",{1},"{2}",{3},"{4}"),'.format(chromosome,
                         position, line[0], line[1], line[2])
-        
+
         query = query[:-1] + ';'
         self.con.execute(query)
         self.con.commit()
