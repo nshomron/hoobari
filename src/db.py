d0939c440d23867c2357d9cef1c56e59d6f8109d
tomr <tomr@mail.tau.ac.il>
2017-08-06 19:32:46 +0300

    new_patch

--- src/db.py
+++ src/db.py
@@ -1,24 +1,30 @@
 import sqlite3
 import csv
 from sys import stderr
+import os
 
 class Variants(object):
     def __init__(self, dropdb=False, dbpath='.'):
         # Connect to DB
-        self.con = sqlite3.connect(dbpath + '/hoobari.db', isolation_level = None, timeout=1000)
-
-        # Drop existing database if needed
-        if dropdb:
-            self.con.execute('DROP TABLE `variants`;')
+        self.con = sqlite3.connect(dbpath, isolation_level = None)
 
         # Check table's existance
         res = self.con.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='variants';")
-        
+
+        # Drop existing database if needed
+        if dropdb and res.fetchone():
+            self.con.execute('DROP TABLE `variants`;')
+
         if not res.fetchone():
-            try:
-                self.con.execute('CREATE TABLE `variants` (`chromosome` char(2) DEFAULT NULL,`pos` int(10) NOT NULL,`genotype` char(20) DEFAULT NULL,`length` int(10) DEFAULT NULL,`qname` varchar(50) DEFAULT NULL)')
-            except sqlite3.OperationalError:
-                pass
+            self.con.execute(   'CREATE TABLE `variants` (' +
+                                '`chromosome` char(2) DEFAULT NULL,' + 
+                                '`pos` int(10) NOT NULL,' +
+                                '`genotype` char(20) DEFAULT NULL,' + 
+                                '`length` int(10) DEFAULT NULL,' +
+                                '`qname` varchar(50) DEFAULT NULL,' + 
+                                '`is_fetal` int(1) DEFAULT NULL,' +
+                                '`var_type` numeric(6) DEFAULT NULL,' +
+                                '`for_ff` int(1) DEFAULT NULL)')
 
     # Insert variants to table
     def insertVariant(self, chromosome, position, info_list):
@@ -29,15 +35,17 @@ class Variants(object):
             `pos`,
             `genotype`,
             `length`,
-            `qname`)
+            `qname`,
+            `is_fetal`,
+            `var_type`,
+            `for_ff`)
             VALUES
             '''
         
         for line in info_list:
-            query += '("{0}",{1},"{2}",{3},"{4}"),'.format(chromosome,
-                        position, line[0], line[1], line[2])
+            query += '("{0}",{1},"{2}",{3},"{4}",{5},{6},{7}),'.format(chromosome,
+                        position, line[0], line[1], line[2], line[3], line[4], line[5])
         
         query = query[:-1] + ';'
         
         self.con.execute(query)
-        self.con.commit()
--- src/freebayes_dd_hoobari_patch.py
+++ src/freebayes_dd_hoobari_patch.py
@@ -2,20 +2,85 @@
 receives err output pf freebayes -dd as stdin, and for every assessed position, prints out the reads supporting each genotype
 '''
 
-from sys import stdin, argv
+from sys import stdin, argv, stderr, exit
 import os
+import re
 import math
 import pysam
-import argparse
+import vcf
 import db
+import argparse
 
 # --------- parse args ---------
 parser = argparse.ArgumentParser()
 parser.add_argument("-b", "--bam_file")
 parser.add_argument("-t", "--tmp_dir", default = 'tmp_hb')
+parser.add_argument("-r", "--region", default = 'region')
+parser.add_argument("-parents_vcf", "--parents_vcf", help = 'bgzipped vcf of parents, indexed by tabix')
+parser.add_argument("-m", "--maternal_sample_name", help = 'maternal sample name as appears in parents vcf')
+parser.add_argument("-p", "--paternal_sample_name", help = 'paternal sample name as appears in parents vcf')
 parser.add_argument("-f", "--drop_db", action = 'store_true', help = 'override variants database')
 args = parser.parse_args()
 # ------------------------------
+
+# --------- functions ---------
+def get_parental_genotypes(parents_reader, maternal_sample_name, paternal_sample_name, chrom, position):
+	n_rec = 0
+	for rec in parents_reader.fetch(chrom, int(position) - 1, int(position)):
+		maternal_gt = rec.genotype(maternal_sample_name).data.GT
+		paternal_gt = rec.genotype(paternal_sample_name).data.GT
+		n_rec += 1
+		if n_rec > 1:
+			exit('more than one parental variant in the same position')
+		
+	return maternal_gt, paternal_gt
+
+def get_reads_tlen(bam_reader, chrom, position):
+	start_pos = max(0, int(position) - 1000)
+	end_pos = min(bam_reader.lengths[bam_reader.references.index(chrom)], int(position) + 1000)
+	bam_records_at_position = bam_reader.fetch(chrom, start_pos, end_pos) # include a flanking region, since there's local realignment
+	tlen_at_position_dic = {}
+	for rec in bam_records_at_position:
+		tlen_at_position_dic[rec.query_name] = rec.template_length
+	return tlen_at_position_dic
+
+def is_fetal_ref(maternal_gt, paternal_gt):
+	if maternal_gt == '0/0' and paternal_gt in ('0/1','1/1'):
+		return False
+	elif maternal_gt == '1/1' and paternal_gt in ('0/0','0/1'):
+		return True
+
+def is_fetal_fragment(geno, vcf_var_line, is_fetal_ref = False):
+	
+	print('is_fetal_ref: ' + str(is_fetal_ref))
+	ref, alt = vcf_var_line.split('\t')[4:6]
+
+	if ((geno == ref) and is_fetal_ref) or ((geno == alt) and not is_fetal_ref):
+		return 1
+	else:
+		return 0
+
+def get_var_type(vcf_line):
+	var_type = re.findall('TYPE=(.*);', vcf_line)[0]
+	if var_type == 'snp':
+		return 1
+	elif var_type == 'mnp':
+		return 2
+	elif var_type == 'ins':
+		return 3
+	elif var_type == 'del':
+		return 4
+	elif var_type == 'complex':
+		return 5
+	else:
+		return 0
+
+def use_for_fetal_fraction_calculation(maternal_gt, paternal_gt, var_type):
+	if (maternal_gt == '0/0' and paternal_gt == '1/1') or (maternal_gt == '1/1' and paternal_gt == '0/0'):
+		return 1
+	else:
+		return 0
+
 
 '''
 This patch uses freebayes' algorithm to create a folders' tree: tmp_folder/jsons/chr[1-22,X,Y].
@@ -32,33 +97,51 @@ Explanation:
 
 # Initiate variants database
 bam_reader = pysam.AlignmentFile(os.path.join(args.bam_file), 'rb')
+parents_reader = vcf.Reader(filename = args.parents_vcf)
+vardb = db.Variants(args.drop_db, dbpath = os.path.join(args.tmp_dir, 'hoobari.' + str(args.region) + '.db'))
 
 for line in stdin:
 	
 	if line.startswith('position: '):
-		initiate_json = True
+		initiate_var = True
 		line = line.split()
 		var = line[1]
 
 	elif line.startswith('haplo_obs'):
-		if initiate_json:
+		if initiate_var:
 			chrom, position = var.split(':')
-			bam_records_at_position = bam_reader.fetch(chrom, int(position) - 1000, int(position) + 1000) # include a flanking region, since there's local realignment
-			template_lengths_at_position_dic = {}
-			for rec in bam_records_at_position:
-				template_lengths_at_position_dic[rec.query_name] = rec.template_length
+			template_lengths_at_position_dic = get_reads_tlen(bam_reader, chrom, position)
 			position_list = []
-			initiate_json = False
-
+			initiate_var = False
 		line = line.rstrip().split('\t')
 		geno = line[3]
 		read = line[4].split(':')
 		qname = ':'.join(read[1:8])
 		isize = int(template_lengths_at_position_dic[qname])
+		
 		position_list.append([geno, math.fabs(isize), qname])
 
 	elif line.startswith('finished position'):
-		vardb = db.Variants(args.drop_db, dbpath = args.tmp_dir)
-		vardb.insertVariant(chrom.replace('chr',''), int(position), position_list)
-		vardb.con.close()
-		initiate_json = True
+		if not initiate_var:
+			maternal_gt, paternal_gt = get_parental_genotypes(	parents_reader,
+										args.maternal_sample_name,
+										args.paternal_sample_name,
+										chrom,
+										position)
+			
+			is_fetal = is_fetal_fragment(geno, former_line, is_fetal_ref = is_fetal_ref(maternal_gt, paternal_gt))
+			var_type = get_var_type(former_line)
+			for_ff = use_for_fetal_fraction_calculation(maternal_gt, paternal_gt, var_type)
+
+			for l in position_list:
+				l += [is_fetal, var_type, for_ff]
+
+			vardb.insertVariant(chrom.replace('chr',''), int(position), position_list)
+			initiate_var = True
+	
+	former_line = line
+
+
+bam_reader.close()
+vardb.con.commit()
+vardb.con.close()
