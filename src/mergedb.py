999659d205b87cadcc94934ec24bc6cbedbee41d
shep <guyspersonal@gmail.com>
2017-08-15 17:45:16 +0300

    Fixed

--- src/mergedb.py
+++ src/mergedb.py
@@ -2,6 +2,16 @@ import sqlite3
 import sys
 import shutil
 
+# Merge the two sqlite DBs with no duplications
+def MergeDBs(db1, db2, table='variants'):
+    con = sqlite3.connect(db1)
+
+    con.execute("""attach '{0}' as toMerge;
+                BEGIN;
+                insert or ignore into {1} select * from toMerge.{1} where not exists (select * from {1} where chromosome=toMerge.chromosome AND pos=toMerge.pos);
+                COMMIT;
+                detach toMerge;""".format(db2,table))
+
 # Get database names
 dbs=[]
 with open(sys.argv[1]) as dbsfile:
@@ -16,12 +26,3 @@ shutil.copyfile(dbs[0],db1)
 for db2 in dbs[1:]:
     MergeDBs(db1,db2)
 
-# Merge the two sqlite DBs with no duplications
-def MergeDBs(db1, db2, table='variants'):
-    con = sqlite3.connect(db1)
-
-    con.execute("""attach '{0}' as toMerge;
-                BEGIN;
-                insert or ignore into {1} select * from toMerge.{1} where not exists (select * from {1} where chromosome=toMerge.chromosome AND pos=toMerge.pos);
-                COMMIT;
-                detach toMerge;""".format(db2,table))
