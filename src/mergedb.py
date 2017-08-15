15df56558645c384bd32e7b5b4c953a05bd32226
shep <guyspersonal@gmail.com>
2017-08-15 17:38:23 +0300

    Added parameters to db merger

--- src/mergedb.py
+++ src/mergedb.py
@@ -1,7 +1,19 @@
 import sqlite3
+import sys
+
+# Get database names
+dbs=[]
+with open(sys.argv[1]) as dbsfile:
+    for line in dbsfile:
+        dbs.append(line)
+db1=dbs[0]
+
+# Merge dbs
+for db2 in dbs[1:]:
+    MergeDBs(db1,db2)
 
 # Merge the two sqlite DBs with no duplications
-def MergeDBs(db1, db2, table):
+def MergeDBs(db1, db2, table='variants'):
     con = sqlite3.connect(db1)
 
     con.execute("""attach '{0}' as toMerge;
