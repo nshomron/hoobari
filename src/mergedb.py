9cb40a5f8f6c56479c9ca9f0a5f45f142656b46d
shep <guyspersonal@gmail.com>
2017-08-15 17:50:26 +0300

    Added script as non-singular command

--- src/mergedb.py
+++ src/mergedb.py
@@ -6,11 +6,12 @@ import shutil
 def MergeDBs(db1, db2, table='variants'):
     con = sqlite3.connect(db1)
 
-    con.execute("""attach '{0}' as toMerge;
-                BEGIN;
-                insert or ignore into {1} select * from toMerge.{1} where not exists (select * from {1} where chromosome=toMerge.chromosome AND pos=toMerge.pos);
-                COMMIT;
-                detach toMerge;""".format(db2,table))
+    con.executescript("""attach '{0}' as toMerge;
+                         BEGIN;
+                         insert or ignore into {1} select * from toMerge.{1}
+                         where not exists (select * from {1} where chromosome=toMerge.chromosome AND pos=toMerge.pos);
+                         COMMIT;
+                         detach toMerge;""".format(db2,table))
 
 # Get database names
 dbs=[]
