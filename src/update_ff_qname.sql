96bc6dce65e72941fdbeb6009668efdefb9ac8eb
Guy <guyspersonal@gmail.com>
2017-09-14 15:33:57 +0300

    Fixed fetal update

--- src/update_ff_qname.sql
+++
@@ -1,2 +0,0 @@
-update variants set for_ff=1 where qname in (select distinct(qname) from variants where for_ff=1);
-update variants set for_ff=2 where qname in (select distinct(qname) from variants where for_ff=2);
