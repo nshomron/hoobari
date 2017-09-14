8529c93dcfa3e0715e209589968fc371f12935b4
Guy Shapira <guyshapira@mail.tau.ac.il>
2017-09-14 17:07:13 +0300

    Fixed scripts

--- src/update_fetal.sql
+++ src/update_fetal.sql
@@ -1,1 +1,1 @@
-update variants set is_fetal=1 where qname in (select distinct(qname) from variants where is_fetal=1);
+update variants set is_fetal=1 where qname in (select qname from variants where is_fetal=1);
