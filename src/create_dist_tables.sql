5afd5e6dc27f2c3cefde7551557ed75e6ffe00f0
Guy <guyspersonal@gmail.com>
2017-09-14 14:18:08 +0300

    Fixed SQLite syntax

--- src/create_dist_tables.sql
+++ src/create_dist_tables.sql
@@ -2,24 +2,28 @@ create table if not exists fetal_lengths(
     `length` int NOT NULL,
     `count`  int NOT NULL DEFAULT '0',
     PRIMARY KEY (`length`)
-)
+);
 
 create table if not exists shared_lengths(
     `length` int NOT NULL,
     `count`  int NOT NULL DEFAULT '0',
     PRIMARY KEY (`length`)
-)
+);
 
 insert into fetal_lengths 
 select `length`, count(*) as `count` 
 from (select min(`length`) as `length` 
       from variants where for_ff=1 and chromosome not in ('X', 'Y') 
       group by `qname`) as qunique 
-group by `length`
+group by `length`;
+
+commit;
 
 insert into shared_lengths 
 select `length`, count(*) as `count` 
 from (select min(`length`) as `length` 
       from variants where for_ff=2 and chromosome not in ('X', 'Y') 
       group by `qname`) as qunique 
-group by `length`
+group by `length`;
+
+commit;
