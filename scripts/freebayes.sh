635e20fc1f017b2a7d74eff99b0e118afe37b32f
Guy Shapira <guyspersonal@gmail.com>
2018-01-17 20:06:43 +0200

    Absolute paths

--- scripts/freebayes.sh
+++ scripts/freebayes.sh
@@ -4,7 +4,7 @@
 #$ -cwd
 #$ -o .$JOB_NAME.$JOB_ID.log
 
-cat /groups/nshomron/guyshapira/tmp/hoobari/debug_trimmed.txt |python3 hoobari/src/freebayes_patch.py \
+cat /groups/nshomron/guyshapira/tmp/hoobari/debug_trimmed.txt |python3 /groups/nshomron/guyshapira/projects/hoobari/src/freebayes_patch.py \
     -b /groups/nshomron/tomr/projects/cffdna/runs/fam01/short/S01.sorted.mdup.bam \
     -parents_vcf /groups/nshomron/guyshapira/tmp/hoobari/short.vcf \
     -m M01 -p F01 -r chr1:0-1000000
