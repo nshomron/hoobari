09dc618a293fb29d094b1f4cd87f1797ad3ee007
Guy Shapira <guyshapira@mail.tau.ac.il>
2018-01-17 20:11:23 +0200

    gzipped vcf

--- scripts/freebayes.sh
+++ scripts/freebayes.sh
@@ -6,5 +6,5 @@
 
 cat /groups/nshomron/guyshapira/tmp/hoobari/debug_trimmed.txt |python3 /groups/nshomron/guyshapira/projects/hoobari/src/freebayes_patch.py \
     -b /groups/nshomron/tomr/projects/cffdna/runs/fam01/short/S01.sorted.mdup.bam \
-    -parents_vcf /groups/nshomron/guyshapira/tmp/hoobari/short.vcf \
+    -parents_vcf /groups/nshomron/guyshapira/tmp/hoobari/short.vcf.gz \
     -m M01 -p F01 -r chr1:0-1000000
